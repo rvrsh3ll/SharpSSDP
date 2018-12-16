045e3c448b17c280c60383f5f4f5c4ec6af0388c
rvrsh3ll <rvrsh3ll@users.noreply.github.com>
2018-12-16 11:14:01 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # SharpSSDP
+
+SharpSSDP is using RSSDP from [RSSDP]https://github.com/Yortw/RSSDP)
+
+
+As an example, one could execute SharpSSDP.exe through Cobalt Strike's Beacon "execute-assembly" module to discover SSDP related services.
+
+
+#### Example usage
+beacon>execute-assembly /root/SharpSSDP/SharpSSDP.exe
+
+![Example](https://raw.githubusercontent.com/rvrsh3ll/SharpSSDP/master/example/example.png)
---
+++ SharpSSDP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Rssdp;
+
+namespace SharpSSDP
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            using (var deviceLocator = new SsdpDeviceLocator())
+            {
+                var foundDevices = await deviceLocator.SearchAsync(); // Can pass search arguments here (device type, uuid). No arguments means all devices.
+
+                foreach (var foundDevice in foundDevices)
+                {
+                    // Device data returned only contains basic device details and location ]
+                    // of full device description.
+                    Console.WriteLine("Found " + foundDevice.Usn + " at " + foundDevice.DescriptionLocation.ToString());
+
+                    // Can retrieve the full device description easily though.
+                    var fullDevice = await foundDevice.GetDeviceInfo();
+                    Console.WriteLine(fullDevice.FriendlyName);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
